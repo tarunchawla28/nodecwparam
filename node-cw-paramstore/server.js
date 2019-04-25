71b4ee298ee48a5838bb138ac7eb8e63bc3dc79c
Tarun Chawla <37270001+tarunchawla28@users.noreply.github.com>
2019-04-25 15:22:33 +0530

    Update server.js

--- node-cw-paramstore/server.js
+++ node-cw-paramstore/server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     logger.log({level:'info', message:{reqMethod: 'GET', primaryKey: 'nothing'}, tags: 'starting'});
     console.log("----received the request11----");
     logger.log('info', 'MURALI Parameter store', {tags: 'finishes'});
-    res.send('Multi test 3008 Paramter test Internal Application!!..\n');
+    res.send('Hi, I am updated with revision_number\n');
 });
 
 app.listen(PORT, HOST);
